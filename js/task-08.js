9514887983482b66d7bdfe943792052994bd647c
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-06-03 16:11:08 +0300

    8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
+formEl.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email;
+  const password = event.currentTarget.elements.password;
+  if (email.value === "" || password.value === "") {
+    const message = "Заповніть поля";
+    alert(message);
+  } else {
+    const data = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(data);
+  }
 
+  formEl.reset();
+}
