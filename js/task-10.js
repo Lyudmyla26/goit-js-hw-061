e267be4156cb00ee79d13f64a1893d6ff190d1af
Lyudmyla26 <124089916+Lyudmyla26@users.noreply.github.com>
2023-06-03 16:47:18 +0300

    m

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,9 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const fomEl = document.querySelector("#controls");
+const input = document.querySelector('[type="number"]');
+const btnOF = document.querySelector("button[data-create]");
+const btnClen = document.querySelector("button[data-v]");
+console.log(input);
+function createBoxes(amount) {}
